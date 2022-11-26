57e751b564bd5dd71c4387fff3c0071e60b62eed
Bradley Woolf <bradleymwoolf@gmail.com>
2022-11-25 17:31:16 -0800

    ♻️ need to add imports from life.rs

---
+++ main.rs
@@ -0,0 +1,16 @@
+// import life structs from life.rs
+
+
+// Path: main.rs
+fn main() {
+    let life = Life {
+        sense: Sense,
+        catch: Catch,
+        consume: Consume,
+        digest: Digest,
+        replicate: Replicate,
+        defend: Defend,
+        move: Move,
+        communicate: Communicate,
+    };
+}
